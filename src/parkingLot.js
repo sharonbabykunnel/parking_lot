e602cbdbad396cb58e96cd261741db329398b8c3
Sharon <sharonkunnel2003@gmail.com>
2024-12-29 11:12:51 +0530

    completed parkinglot

---
+++ src/parkingLot.js
@@ -0,0 +1,67 @@
+class ParkingLot {
+    constructor(slots) {
+        this.maxSlots = slots;
+        this.slots = new Array(slots).fill(null);
+    }
+
+    park(registrationNo, color) {
+        const nextSlot = this.slots.findIndex(slot => slot === null);
+
+        if (nextSlot === -1) {
+            return 'Sorry parking is fulll';
+        }
+
+        this.slots[nextSlot] = {
+            registrationNo,
+            color
+        };
+
+        return `Allocated slot number: ${nextSlot+1}`;
+    }
+
+    leave(slotNumber) {
+        if (slotNumber < 1 || slotNumber > this.maxSlots) {
+            return "Invalid slot number";
+        }
+
+        if (this.slots[slotNumber - 1] === null) {
+            return "Slot already empty";
+        }
+
+        this.slots[slotNumber - 1] = null;
+        return `Slot number ${slotNumber} is free`;
+    }
+
+    status() {
+        let output = 'Slot No.\tRegistration No.\TColor\n';
+        this.slots.forEach((slot, index) => {
+            if (slot !== null) {
+                output += `${index + 1}\t${slot.registrationNo}\t${slot.color}\n`
+            }
+        });
+        return output;
+    }
+
+    getRegistrationNumbersForColor(color) {
+        const registrationNumber = this.slots
+            .filter(slot => slot !== null && slot.color.toLowerCase() === color.toLowerCase())
+            .map(slot => slot.registrationNo);
+        return registrationNumber.length > 0 ? registrationNumber.join(' ') : "Not found";
+    }
+
+    getSlotNumberForColor(color) {
+        const slotNumbers = this.slots
+          .map((slot, index) =>
+            slot !== null && slot.color.toLowerCase() === color.toLowerCase()
+              ? index + 1
+              : null
+          )
+            .filter((slot) => slot !== null);
+        return slotNumbers.length > 0 ? slotNumbers.join(',') : "Not found";
+    }
+
+    getSlotNumberForRegistration(registrationNo) {
+        const slotNumber = this.slots.findIndex(slot => slot !== null && slot.registrationNo === registrationNo);
+        return slotNumber !== -1 ? (slotNumber + 1).toString() : "Not found";
+    }
+}
